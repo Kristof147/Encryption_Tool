bcb5537c65aafc711ee2a09921b6b6e76b432c60
Seppe Baerts <12202625@student.pxl.be>
2024-03-11 10:30:54 +0100

    Created the BaseCryptor => This is used for the base of any encryptor Created RSACryptor => This is the RSA encryptor Created CryptoParameters => These will get the supported parameters, you can get the parameters inside your baseCryptor with "GetParameters(out ...)" Created CryptoEngine => This will be called and will handle the creation of the baseCryptors.

---
+++ Encryption_Tool/Crypto/models/CryptoParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Tool.EncryptionEngine.models
+{
+    public class CryptoParameters
+    {
+        public RSAParameters? RSAParameters { get; set; }
+        public RSAEncryptionPadding? RSAEncryptionPadding { get; set; }
+        public byte[]? Key { get; set; }
+        public byte[]? IV { get; set; }
+        public bool GetParameters(out RSAParameters rsaParams, out RSAEncryptionPadding padding)
+        {
+            rsaParams = new RSAParameters();
+            padding = RSAEncryptionPadding;
+            if (RSAParameters == null)
+                return false;
+            
+            rsaParams = (RSAParameters)RSAParameters;
+            return true;
+        }
+        public bool GetParameters(out byte[] key, out byte[] iv)
+        {
+            key = [];
+            iv = [];
+            if (Key == null || IV == null)
+                return false;
+            
+            key = Key;
+            iv = IV;
+            return true;
+        }
+    }
+}
--- Encryption_Tool/MainWindow.xaml.cs
+++ Encryption_Tool/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Encryption_Tool.EncryptionEngine.models;
+using Encryption_Tool.Service;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +22,39 @@ namespace Encryption_Tool
 		public MainWindow()
 		{
 			InitializeComponent();
+			EncryptionEngine.CryptoEngine engine = new();
+			RSAParameters encryptionParameters = new();
+			RSAParameters decryptionParameters = new();
+			RSAEncryptionPadding padding = RSAEncryptionPadding.CreateOaep(HashAlgorithmName.SHA256);
+			using (RSA Rsa = RSA.Create())
+			{
+				encryptionParameters = Rsa.ExportParameters(false);
+				decryptionParameters = Rsa.ExportParameters(true);
+			}
+			UnicodeEncoding byteConverter = new();
+			byte[] dataToEncrypt = byteConverter.GetBytes("This is a test");
+			CryptoParameters parameters = new()
+			{
+				RSAParameters = encryptionParameters,
+				RSAEncryptionPadding = padding,
+			};
+			EncryptionRequest request = new()
+			{
+                DataToEncrypt = dataToEncrypt,
+				EncryptionType = EncryptionType.RSA,
+				Parameters = parameters
+            };
+			var response = engine.Encrypt(request);
+			parameters.RSAParameters = decryptionParameters;
+			DecryptionRequest decryptRequest = new()
+			{
+				DataToDecrypt = response.Data,
+				EncryptionType = EncryptionType.RSA,
+				Parameters = parameters
+			};
+			var decryptResponse = engine.Decryption(decryptRequest);
+			string decryptedData = byteConverter.GetString(decryptResponse.Data);
+			_ = 0;
 		}
 	}
 }
