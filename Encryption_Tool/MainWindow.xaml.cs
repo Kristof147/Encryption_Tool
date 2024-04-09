ac51bbff7aa28016c9cdf99a35d6989f3b7b4089
Kristof Dello <kristof.dello@student.pxl.be>
2024-04-09 19:01:25 +0200

    Add method to go to and from Byte[] for Images

--- Encryption_Tool/MainWindow.xaml.cs
+++ Encryption_Tool/MainWindow.xaml.cs
@@ -22,124 +22,148 @@ namespace Encryption_Tool
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-      
-        private Dictionary<string, string>? aesKeysDict;
-        private readonly FileManager fm;
-        string keyDirectoryPath = @"C:\test";
-        string imageDirectoryPath = @"C:\test";
-        string aESDirectoryPath = @"C:\test";
-        public MainWindow()
-        {
-            InitializeComponent();
-            InitializeKeys();
-            fm = new FileManager();
-        }
 
-        private void btnKeyGeneration_Click(object sender, RoutedEventArgs e)
-        {
-            string publicKey;
-            string privateKey;
+		private Dictionary<string, string>? aesKeysDict;
+		private readonly FileManager fm;
+		string keyDirectoryPath = @"C:\test";
+		string imageDirectoryPath = @"C:\test";
+		string aESDirectoryPath = @"C:\test";
+		public MainWindow()
+		{
+			InitializeComponent();
+			InitializeKeys();
+			fm = new FileManager();
+		}
 
-            KeyHelper.SaveKeys(out publicKey, out privateKey);
-            //txtPublicKey.Text = publicKey;
-            //txtPrivateKey.Text = privateKey;
-        }
+		private void btnKeyGeneration_Click(object sender, RoutedEventArgs e)
+		{
+			string publicKey;
+			string privateKey;
 
-
-        private void InitializeKeys()
-        {
-            aesKeysDict = new Dictionary<string, string>();
-       
-
-            if (Directory.Exists(keyDirectoryPath))
-            {
-                string[] files = Directory.GetFiles(keyDirectoryPath, "*.txt");
-
-                foreach (string file in files)
-                {
-                    string fileName = Path.GetFileName(file);
-                    string base64Key = File.ReadAllText(file).Trim(); 
-
-                    aesKeysDict.Add(fileName, base64Key);
-                    CmbAESKeys.Items.Add(fileName); 
-                }
-            }
-           
-        }
-        #region key generation tab
-        private void BtnFolderKey_Click(object sender, RoutedEventArgs e)
-        {
-            string selectedDirectory = fm.SelectDirectory();
-            if (!string.IsNullOrEmpty(selectedDirectory))
-            {
-                keyDirectoryPath = selectedDirectory;
-                InitializeKeys();
-            }
-        }
-
-        private void BtnAES_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void BtnRSAPair_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+			KeyHelper.SaveKeys(out publicKey, out privateKey);
+			//txtPublicKey.Text = publicKey;
+			//txtPrivateKey.Text = privateKey;
+		}
 
 
+		private void InitializeKeys()
+		{
+			aesKeysDict = new Dictionary<string, string>();
 
-        #endregion
-        #region AES tab
-        private void BtnAESFolder_Click(object sender, RoutedEventArgs e)
-        {
-            string selectedDirectory = fm.SelectDirectory();
-            if (!string.IsNullOrEmpty(selectedDirectory))
-            {
-                imageDirectoryPath = selectedDirectory;
-                
-            }
-        }
 
-        private void BtnAESEncrypt_Click(object sender, RoutedEventArgs e)
-        {
+			if (Directory.Exists(keyDirectoryPath))
+			{
+				string[] files = Directory.GetFiles(keyDirectoryPath, "*.txt");
 
-        }
+				foreach (string file in files)
+				{
+					string fileName = Path.GetFileName(file);
+					string base64Key = File.ReadAllText(file).Trim();
 
-        private void BtnAESDecrypt_Click(object sender, RoutedEventArgs e)
-        {
+					aesKeysDict.Add(fileName, base64Key);
+					CmbAESKeys.Items.Add(fileName);
+				}
+			}
 
-        }
+		}
+		#region key generation tab
+		private void BtnFolderKey_Click(object sender, RoutedEventArgs e)
+		{
+			string selectedDirectory = fm.SelectDirectory();
+			if (!string.IsNullOrEmpty(selectedDirectory))
+			{
+				keyDirectoryPath = selectedDirectory;
+				InitializeKeys();
+			}
+		}
 
-        private void BtnAESImage_Click(object sender, RoutedEventArgs e)
-        {
+		private void BtnAES_Click(object sender, RoutedEventArgs e)
+		{
 
-        }
-        #endregion
-        #region RSA tab
-        private void BtnRSAEncrypt_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+		}
+		private void BtnRSAPair_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void BtnRSADecrypt_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
 
-        private void BtnRSAImage_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
+		#endregion
+		#region AES tab
+		private void BtnAESFolder_Click(object sender, RoutedEventArgs e)
+		{
+			string selectedDirectory = fm.SelectDirectory();
+			if (!string.IsNullOrEmpty(selectedDirectory))
+			{
+				imageDirectoryPath = selectedDirectory;
 
-        private void BtnRSAFolder_Click(object sender, RoutedEventArgs e)
-        {
-            string selectedDirectory = fm.SelectDirectory();
-            if (!string.IsNullOrEmpty(selectedDirectory))
-            {
-                aESDirectoryPath = selectedDirectory;
+			}
+		}
 
-            }
-        }
-#endregion
-    }
+		private void BtnAESEncrypt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BtnAESDecrypt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BtnAESImage_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+		#endregion
+		#region RSA tab
+		private void BtnRSAEncrypt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BtnRSADecrypt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BtnRSAImage_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BtnRSAFolder_Click(object sender, RoutedEventArgs e)
+		{
+			string selectedDirectory = fm.SelectDirectory();
+			if (!string.IsNullOrEmpty(selectedDirectory))
+			{
+				aESDirectoryPath = selectedDirectory;
+
+			}
+		}
+		#endregion
+
+		#region ImageToAndFromByteArray
+		byte[] ImageToByteArray(string path)
+		{
+			return File.ReadAllBytes(path);
+		}
+
+		private BitmapImage ByteArrayToImage(byte[] data)
+		{
+			BitmapImage returnImage = new BitmapImage();
+			Application.Current.Dispatcher.Invoke(() =>
+			{
+				MemoryStream stream = new MemoryStream();
+				stream.Write(data, 0, data.Length);
+				stream.Position = 0;
+				returnImage.BeginInit();
+				returnImage.CacheOption = BitmapCacheOption.OnLoad;
+				returnImage.StreamSource = stream;
+				returnImage.EndInit();
+				returnImage.Freeze(); // Zorgt ervoor dat de afbeelding niet verandert nadat het is geladen
+			});
+			return returnImage;
+		}
+		#endregion
+	}
 }
